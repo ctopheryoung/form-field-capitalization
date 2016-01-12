9d9e9fbc16d2b906a2e33eaf1262e45fc448d6a8
Blake Scott and Chris Young <blakerscott@gmail.com>
2016-01-12 14:54:05 -0800

    add headings, add capitalization functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     var response = $("input#response").val();
+    response = response.toUpperCase();
+    console.log(response);
     $(".response").text(response);
 
     event.preventDefault();
